f89d1e0a3132d225e4d100680b4c873344aea73d
Fazil Husain <98115662+FazilHusain@users.noreply.github.com>
2024-09-26 18:08:53 +0530

    Update GlobalState.js

--- src/GlobalState.js
+++ src/GlobalState.js
@@ -9,7 +9,7 @@ export const DataProvider = ({ children }) => {
   const [token, setToken] = useState(false);
 
   const refreshToken = async() => {
-    const res = await axios.get('https://e-comm-backend-3-6r0t.onrender.com/user/refresh_token');
+    const res = await axios.get('/user/refresh_token');
     setToken(res.data.accesstoken);
   }
   
