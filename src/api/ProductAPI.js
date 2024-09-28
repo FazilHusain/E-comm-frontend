48b39a481e41a52916cd5c217d61153cd1a7c17c
Fazil Husain <98115662+FazilHusain@users.noreply.github.com>
2024-09-28 14:23:08 +0530

    Update ProductAPI.js

--- src/api/ProductAPI.js
+++ src/api/ProductAPI.js
@@ -6,7 +6,7 @@ const ProductAPI = () => {
     const [products,setProducts]=useState([])
 
     const getProducts = async()=> {
-        const res = await axios.get("https://e-comm-backend-6.onrender.com/api/products")
+        const res = await axios.get("https://e-comm-backend-z6qo.onrender.com/api/products")
         setProducts(res.data.products)
     }
 
